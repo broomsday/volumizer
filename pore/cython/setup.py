66959a2e227b91373e9ca1ab3d79d0b9dcb4c9b5
Aron Broom <aron@proteinqure.com>
2021-10-31 16:43:02 -0400

    Remove cython and cythonized attempt at voxel search

--- pore/cython/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-from pore.paths import ROOT_DIR
-
-setup(ext_modules = cythonize(str(ROOT_DIR / "pore" / "cython" / "cython_voxel.pyx")))
