de47282a8ed74219dffad51bf67c332ca63991b5
Aron Broom <aron@proteinqure.com>
2021-10-31 23:57:03 -0400

    Pore vs. cavity or pocket indenfication working

--- pore/pdb.py
+++ pore/pdb.py
@@ -257,29 +257,30 @@ def get_exposed_and_buried_solvent_voxels(
     )
 
 
-def is_neighbor_voxel(voxels, reference_index, query_index) -> bool:
+def is_neighbor_voxel(voxel_one, voxel_two) -> bool:
+    # def is_neighbor_voxel(voxels, reference_index, query_index) -> bool:
     """
     Given two voxels return True if they are ordinal neighbors.
     """
-    reference_voxel = (
-        voxels[0][reference_index],
-        voxels[1][reference_index],
-        voxels[2][reference_index],
-    )
-    query_voxel = (
-        voxels[0][query_index],
-        voxels[1][query_index],
-        voxels[2][query_index],
-    )
-
-    # a voxel is an ordinal neighbor when the sum of the absolute differences in axes indices is exactly 1
     differences = 0
     for dimension in range(3):
-        differences += abs(reference_voxel[dimension] - query_voxel[dimension])
+        differences += abs(voxel_one[dimension] - voxel_two[dimension])
 
+    # a voxel is an ordinal neighbor when the sum of the absolute differences in axes indices is exactly 1
     if differences == 1:
         return True
     return False
+
+
+def get_single_voxel(voxels: tuple[np.ndarray, ...], index: int) -> tuple[np.int64, ...]:
+    """
+    Given a set of voxels return just one voxel at index.
+    """
+    return (
+        voxels[0][index],
+        voxels[1][index],
+        voxels[2][index],
+    )
 
 
 def breadth_first_search(voxels: tuple[np.ndarray, ...], searchable_indices: set[int]) -> set[int]:
@@ -287,14 +288,17 @@ def breadth_first_search(voxels: tuple[np.ndarray, ...], searchable_indices: set
     Given a set of voxels and list of possible indices to add,
     add indices for all ordinal neighbors iteratively until no more such neighbors exist.
     """
+    searchable_indices = deepcopy(searchable_indices)
     start_index = searchable_indices.pop()
     queue_indices = set([start_index])
     neighbor_indices = set([start_index])
 
     while len(queue_indices) > 0:
         current_index = queue_indices.pop()
+        current_voxel = get_single_voxel(voxels, current_index)
         for searched_index in searchable_indices:
-            if is_neighbor_voxel(voxels, current_index, searched_index):
+            searched_voxel = get_single_voxel(voxels, searched_index)
+            if is_neighbor_voxel(current_voxel, searched_voxel):
                 queue_indices.add(searched_index)
                 neighbor_indices.add(searched_index)
         searchable_indices -= neighbor_indices
@@ -302,39 +306,88 @@ def breadth_first_search(voxels: tuple[np.ndarray, ...], searchable_indices: set
     return neighbor_indices
 
 
-def get_pore_voxels(
+def is_pore(
+    putative_pore_indices: set[int], buried_voxels: tuple[np.ndarray, ...], exposed_voxels: tuple[np.ndarray, ...]
+) -> bool:
+    """
+    Find the voxels that lie on the putative pore surface in direct contact with an exposed voxel.
+    If these surface voxels can be agglomerated (BFS) into a single unit, this is a cavity, otherwise a pore.
+    """
+    # TODO function needs significant cleanup
+
+    SURFACE_NEIGHBOR_DISTANCE = 1   # TODO put this constant somewhere else
+
+    direct_surface_indices = set()
+    for putative_pore_index in putative_pore_indices:
+        putative_pore_voxel = get_single_voxel(buried_voxels, putative_pore_index)
+        for exposed_voxel_index in range(len(exposed_voxels[0])):
+            exposed_voxel = get_single_voxel(exposed_voxels, exposed_voxel_index)
+            if is_neighbor_voxel(putative_pore_voxel, exposed_voxel):
+                direct_surface_indices.add(putative_pore_index)
+
+    # after collecting these, add all direct putative pore neighbours (could be adjustable)
+    neighbor_surface_indices = set()
+    for surface_index in direct_surface_indices:
+        surface_voxel = get_single_voxel(buried_voxels, surface_index)
+        for putative_pore_index in putative_pore_indices - direct_surface_indices:
+            putative_pore_voxel = get_single_voxel(buried_voxels, putative_pore_index)
+            # TODO: need to add a dimension value to neighbour check and use SURFACE_NEIGHBOR_DISTANCE
+            if is_neighbor_voxel(surface_voxel, putative_pore_voxel):
+                neighbor_surface_indices.add(putative_pore_index)
+
+    # agglomerate and if that connects everything it's a cavity, otherwise pore
+    surface_indices = direct_surface_indices.union(neighbor_surface_indices)
+    if len(surface_indices) == 0:
+        # TODO: why does this happen? Is this an internal void?  Maybe list them as something different to be ignored
+        return False
+
+    single_surface_indices = breadth_first_search(buried_voxels, surface_indices)
+    if len(single_surface_indices) == len(surface_indices):
+        return False
+
+    return True
+
+
+def get_pore_and_cavity_voxels(
     buried_solvent_voxels: tuple[np.ndarray, ...], exposed_solvent_voxels: tuple[np.ndarray, ...]
-) -> dict[int, tuple[np.ndarray, ...]]:
+) -> tuple[dict[int, tuple[np.ndarray, ...]], dict[int, tuple[np.ndarray, ...]]]:
     """
     Agglomerate buried solvent voxels into putative pores.
 
-    TODO: Then test which putative pores traverse the box.
+    Then test which putative pores traverse the box.
     """
-    # TODO rename function as it will return pore and cavity voxels
-    # TODO some problem with the chains being returned here?
     buried_indices = set(range(buried_solvent_voxels[0].size))
     agglomerated_indices = set()
 
-    putative_pores = {}
-    putative_pore_id = 0
+    pores = {}
+    pore_id = 0
+    cavities = {}
+    cavity_id = 0
     while len(agglomerated_indices) < len(buried_indices):
         remaining_indices = buried_indices - agglomerated_indices
 
-        # perform BFS over the remaining indices
-        putative_pore_indices = breadth_first_search(buried_solvent_voxels, deepcopy(remaining_indices))
+        # perform BFS over the remaining indices (TODO: BFS is currently slow, needs optimizing)
+        putative_pore_indices = breadth_first_search(buried_solvent_voxels, remaining_indices)
         # iterate our counter of finished indices
         agglomerated_indices = agglomerated_indices.union(putative_pore_indices)
-        # define the pore
-        putative_pores[putative_pore_id] = (
-            np.array([buried_solvent_voxels[0][index] for index in putative_pore_indices]),
-            np.array([buried_solvent_voxels[1][index] for index in putative_pore_indices]),
-            np.array([buried_solvent_voxels[2][index] for index in putative_pore_indices]),
-        )
-        putative_pore_id += 1
 
-    # TODO add in test for box traversal to identify pores vs. cavities
+        # identify if this is in fact a pore or just a cavity
+        if is_pore(putative_pore_indices, buried_solvent_voxels, exposed_solvent_voxels):
+            pores[pore_id] = (
+                np.array([buried_solvent_voxels[0][index] for index in putative_pore_indices]),
+                np.array([buried_solvent_voxels[1][index] for index in putative_pore_indices]),
+                np.array([buried_solvent_voxels[2][index] for index in putative_pore_indices]),
+            )
+            pore_id += 1
+        else:
+            cavities[cavity_id] = (
+                np.array([buried_solvent_voxels[0][index] for index in putative_pore_indices]),
+                np.array([buried_solvent_voxels[1][index] for index in putative_pore_indices]),
+                np.array([buried_solvent_voxels[2][index] for index in putative_pore_indices]),
+            )
+            cavity_id += 1
 
-    return putative_pores
+    return pores, cavities
 
 
 def make_atom_line(
@@ -342,20 +395,24 @@ def make_atom_line(
     exposed_solvent_voxel_indices: set[int],
     buried_solvent_voxel_indices: set[int],
     pore_voxel_index_map: dict[int, int],
+    cavity_voxel_index_map: dict[int, int],
     index: int,
 ) -> str:
     """
     Make each exposed solvent point a hydrogen, buried solvent ponit an oxygen, and protein point a carbon
     """
+    # TODO cavities should have an index also for their residue number
     if index in exposed_solvent_voxel_indices:
-        return f"ATOM  {index:>5d}  H   EXP A   1    {point[0]:>8.3f}{point[1]:>8.3f}{point[2]:>8.3f}  1.00  0.00           H"
+        return f"ATOM  {index:>5d}  H   EXP B   1    {point[0]:>8.3f}{point[1]:>8.3f}{point[2]:>8.3f}  1.00  0.00           H"
     elif index in buried_solvent_voxel_indices:
         if index in list(pore_voxel_index_map.keys()):
-            return f"ATOM  {index:>5d}  O   POR D{pore_voxel_index_map[index]:>4d}    {point[0]:>8.3f}{point[1]:>8.3f}{point[2]:>8.3f}  1.00  0.00           O"
+            return f"ATOM  {index:>5d}  O   POR E{pore_voxel_index_map[index]:>4d}    {point[0]:>8.3f}{point[1]:>8.3f}{point[2]:>8.3f}  1.00  0.00           O"
+        elif index in list(cavity_voxel_index_map.keys()):
+            return f"ATOM  {index:>5d}  N   CAV D{cavity_voxel_index_map[index]:>4d}    {point[0]:>8.3f}{point[1]:>8.3f}{point[2]:>8.3f}  1.00  0.00           N"
         else:
-            return f"ATOM  {index:>5d}  N   BUR B   1    {point[0]:>8.3f}{point[1]:>8.3f}{point[2]:>8.3f}  1.00  0.00           N"
+            return f"ATOM  {index:>5d}  H   BUR C   1    {point[0]:>8.3f}{point[1]:>8.3f}{point[2]:>8.3f}  1.00  0.00           N"
     else:
-        return f"ATOM  {index:>5d}  C   PTN C   1    {point[0]:>8.3f}{point[1]:>8.3f}{point[2]:>8.3f}  1.00  0.00           C"
+        return f"ATOM  {index:>5d}  C   PTN A   1    {point[0]:>8.3f}{point[1]:>8.3f}{point[2]:>8.3f}  1.00  0.00           C"
 
 
 def compute_voxel_indices(voxels: tuple[np.ndarray, ...], grid_dimensions: np.ndarray) -> set[int]:
@@ -373,6 +430,7 @@ def points_to_pdb(
     exposed_solvent_voxels: tuple[np.ndarray, ...],
     buried_solvent_voxels: tuple[np.ndarray, ...],
     pore_voxels: dict[int, tuple[np.ndarray, ...]],
+    cavity_voxels: dict[int, tuple[np.ndarray, ...]],
 ) -> None:
     """
     Write out points as though it was a PDB file.
@@ -384,15 +442,27 @@ def points_to_pdb(
     exposed_solvent_voxel_indices = compute_voxel_indices(exposed_solvent_voxels, voxel_grid.x_y_z)
     buried_solvent_voxel_indices = compute_voxel_indices(buried_solvent_voxels, voxel_grid.x_y_z)
     pore_voxel_indices = {i: compute_voxel_indices(voxels, voxel_grid.x_y_z) for i, voxels in pore_voxels.items()}
+    cavity_voxel_indices = {i: compute_voxel_indices(voxels, voxel_grid.x_y_z) for i, voxels in cavity_voxels.items()}
 
-    # TODO change name of this next thing and possibly improve how made
     pore_voxel_index_map = {}
     for id, indices in pore_voxel_indices.items():
         for index in indices:
             pore_voxel_index_map[index] = id
 
+    cavity_voxel_index_map = {}
+    for id, indices in cavity_voxel_indices.items():
+        for index in indices:
+            cavity_voxel_index_map[index] = id
+
     output_lines = [
-        make_atom_line(point, exposed_solvent_voxel_indices, buried_solvent_voxel_indices, pore_voxel_index_map, i)
+        make_atom_line(
+            point,
+            exposed_solvent_voxel_indices,
+            buried_solvent_voxel_indices,
+            pore_voxel_index_map,
+            cavity_voxel_index_map,
+            i,
+        )
         for i, point in enumerate(voxel_grid.voxel_centers)
     ]
 
@@ -423,7 +493,7 @@ def process_one_pdb(pdb_id: str) -> bool:
     exposed_solvent_voxels, buried_solvent_voxels = get_exposed_and_buried_solvent_voxels(
         solvent_voxels, protein_voxels, voxel_grid.x_y_z
     )
-    pore_voxels = get_pore_voxels(buried_solvent_voxels, exposed_solvent_voxels)
+    pore_voxels, cavity_voxels = get_pore_and_cavity_voxels(buried_solvent_voxels, exposed_solvent_voxels)
 
     print("\n")
     print(pdb_id)
@@ -433,8 +503,9 @@ def process_one_pdb(pdb_id: str) -> bool:
     print("Exposed Solvent Voxels:", exposed_solvent_voxels[0].size)
     print("Buried Solvent Voxels:", buried_solvent_voxels[0].size)
     print("Pores:", len(pore_voxels))
+    print("Cavities:", len(cavity_voxels))
 
-    points_to_pdb(voxel_grid, exposed_solvent_voxels, buried_solvent_voxels, pore_voxels)
+    points_to_pdb(voxel_grid, exposed_solvent_voxels, buried_solvent_voxels, pore_voxels, cavity_voxels)
 
     quit()
     return False
