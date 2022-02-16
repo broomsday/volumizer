cb30656fb97a1186c6d7284730cae9febf5624c7
Aron Broom <broomsday@gmail.com>
2022-02-16 17:27:58 -0500

    Draft: find seannotations by metrics

--- pore/analysis.py
+++ pore/analysis.py
@@ -5,6 +5,8 @@ Module holding functions for post poration analysis of volume objects.
 
 from pathlib import Path
 
+import pandas as pd
+
 from pore.paths import ANNOTATED_DF_DIR
 from pore.utils import VOXEL_SIZE
 
@@ -24,3 +26,46 @@ def get_annotations_by_id(pdb_ids: list[str]) -> tuple[list[Path], int]:
     annotation_paths = [annotation_path for annotation_path in annotation_paths if annotation_path is not None]
 
     return annotation_paths, missing_annotations
+
+
+def get_pdb_annotations(annotation_paths: list[Path]) -> dict[str, pd.DataFrame]:
+    """
+    Create a dictionary of annotation dataframes keyed by the PDB ID and resolution.
+    """
+    return {annotation_path.stem: pd.read_json(annotation_path) for annotation_path in annotation_paths}
+
+
+def annotation_has_types(annotation: pd.DataFrame, pores: bool, pockets: bool, cavities: bool) -> bool:
+    """
+    If the given annotation contains any of pores/pockets/cavities that are True, return True.
+    """
+    if annotation.empty:
+        return False
+
+    accepted_types = []
+    if pores:
+        accepted_types.append("pore")
+    if pockets:
+        accepted_types.append("pocket")
+    if cavities:
+        accepted_types.append("cavity")
+
+    for accepted_type in accepted_types:
+        if accepted_type in list(annotation["type"]):
+            return True
+
+    return False
+
+
+def select_annotations_by_type(
+    pdb_annotations: dict[str, pd.DataFrame], pores: bool, pockets: bool, cavities: bool
+) -> dict[str, pd.DataFrame]:
+    """
+    Subset the input dictionary into only those where the value annotation has at least one type
+    matching at least one of pores/pockets/cavities that are True
+    """
+    return {
+        pdb: annotation
+        for pdb, annotation in pdb_annotations.items()
+        if annotation_has_types(annotation, pores, pockets, cavities)
+    }
--- scripts/analysis/get_pdbs_by_metrics.py
+++ scripts/analysis/get_pdbs_by_metrics.py
@@ -31,6 +31,9 @@ def main(
     Scan over annotated DFs for pores, pockets, and/or cavities matching given
     metric constraints.
     """
+    if (not find_pores) and (not find_pockets) and (not find_cavities):
+        warnings.warn("You have not selected any volume types to find!")
+
     input_type = guess_analysis_input_type(analysis_input)
     if input_type == "file":
         with open(analysis_input, mode="r", encoding="utf-8") as id_file:
@@ -45,8 +48,21 @@ def main(
     else:
         raise RuntimeError("Input type not implemented")
 
-    print(annotation_paths)
-    print(len(annotation_paths))
+    pdb_annotations = analysis.get_pdb_annotations(annotation_paths)
+    selected_pdb_annotations = analysis.select_annotations_by_type(
+        pdb_annotations, find_pores, find_pockets, find_cavities
+    )
+
+    metrics = {
+        "min_volume": min_volume,
+        "max_volume": max_volume,
+        "min_dimension_one": min_dimension_one,
+        "max_dimension_one": max_dimension_one,
+    }
+    selected_pdb_annotations = analysis.select_annotations_by_metrics(selected_pdb_annotations, metrics)
+
+    print(selected_pdb_annotations[list(pdb_annotations.keys())[0]])
+    print(len(selected_pdb_annotations))
 
 
 if "__main__" in __name__:
